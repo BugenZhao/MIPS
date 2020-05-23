8552a5d909c2e41adace412ae1ba70cfc089b6da
BugenZhao <i@bugenzhao.com>
2020-05-24 00:24:58 +0800

    Added: headers

--- Includes/Debug.v
+++ Includes/Debug.v
@@ -1,3 +1,9 @@
+// -------------------------------------------------------
+// -- Debug.v - Debug utilities
+// -------------------------------------------------------
+// Bugen Zhao 2020
+// -------------------------------------------------------
+
 `define assert(signal, value) if (signal != value) $warning("ASSERTION FAILED: signal(%08X) != value(%08X)", signal, value);
 
 `define iverilog_init \
