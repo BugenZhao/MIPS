5d8dbe4da275baba3604c90c2544ae6f2d555765
BugenZhao <i@bugenzhao.com>
2020-05-24 00:23:16 +0800

    Added: Alu, AluFunct

---
+++ Sources/Alu.v
@@ -0,0 +1,52 @@
+`include "ISA.v"
+
+module Alu(
+           input wire [31:0] opA, opB,
+           input wire [ 5:0] aluFunct,
+           output reg [31:0] out,
+           output reg        zero
+       );
+
+always @(*) begin
+    case (aluFunct)
+        `FUN_ADD:
+            out = opA + opB;
+        `FUN_ADDU:
+            out = opA + opB;
+        `FUN_SUB:
+            out = opA - opB;
+        `FUN_SUBU:
+            out = opA - opB;
+        `FUN_SLT:
+            out = $signed(opA) < $signed(opB) ? 1 : 0;
+        `FUN_SLTU:
+            out = opA < opB ? 1 : 0;
+        `FUN_AND:
+            out = opA & opB;
+        `FUN_OR:
+            out = opA | opB;
+        `FUN_XOR:
+            out = opA ^ opB;
+        `FUN_NOR:
+            out = ~(opA | opB);
+        `FUN_SLL, `FUN_SLLV:
+            out = opA << opB;
+        `FUN_SRL, `FUN_SRLV:
+            out = opA >> opB;
+        `FUN_SRA, `FUN_SRAV:
+            out = opA >>> opB;
+        `FUN_JR:
+            out = opA + opB; // opB == 0
+        `FUN_NO:
+            out = 0;
+
+        default: begin
+            $warning("%m: aluFunct not recognized: %06b", aluFunct);
+            out = 0;
+        end
+    endcase
+
+    zero = out == 0 ? 1 : 0;
+end
+
+endmodule // Alu
---
+++ Sources/AluFunct.v
@@ -0,0 +1,47 @@
+`include "ISA.v"
+
+module AluFunct(
+           input wire [5:0] opcode,
+           input wire [5:0] funct,
+           output reg [5:0] aluFunct
+       );
+
+always @(*) begin
+    case (opcode)
+        `OPC_SPECIAL:
+            aluFunct = funct;
+        `OPC_ADDI:
+            aluFunct = `FUN_ADD;
+        `OPC_ADDIU:
+            aluFunct = `FUN_ADDU;
+        `OPC_ANDI:
+            aluFunct = `FUN_AND;
+        `OPC_ORI:
+            aluFunct = `FUN_OR;
+        `OPC_XORI:
+            aluFunct = `FUN_XOR;
+        `OPC_LUI:
+            aluFunct = `FUN_NO;
+        `OPC_SLTI:
+            aluFunct = `FUN_SLT;
+        `OPC_SLTIU:
+            aluFunct = `FUN_SLTU;
+        `OPC_REGIMM: // BGEZ, BGEZAL, BLTZ, BLTZAL, (BAL)
+            aluFunct = `FUN_SLT;
+        `OPC_BGTZ, `OPC_BLEZ:
+            aluFunct = `FUN_SLT;
+        `OPC_BEQ, `OPC_BNE:
+            aluFunct = `FUN_SUB;
+        `OPC_LB, `OPC_LW, `OPC_SB, `OPC_SW:
+            aluFunct = `FUN_ADD;
+        `OPC_J, `OPC_JAL:
+            aluFunct = `FUN_NO;
+
+        default: begin
+            $warning("%m: opcode not recognized: %06b", opcode);
+            aluFunct = `FUN_NO;
+        end
+    endcase
+end
+
+endmodule // AluFunct
