ad67ff44b2867e63715e2dd7f8ba47b71c3b01f2
BugenZhao <i@bugenzhao.com>
2020-05-25 01:31:46 +0800

    Updated: test bench for JALR

--- Tests/System_tb.v
+++ Tests/System_tb.v
@@ -15,19 +15,18 @@ parameter PERIOD   = 10;
 
 System #(textDump, PERIOD) u_System();
 
+`define memFile u_System.u_DataMemory.memFile
+`define regFile u_System.u_CPU.u_RegisterFile.regFile
+
 initial begin: test
     integer i;
 
     $dumpfile("wave.vcd");
     $dumpvars;
-    $dumpvars(0, u_System.u_CPU.u_RegisterFile.regFile[4]);
-    $dumpvars(0, u_System.u_CPU.u_RegisterFile.regFile[8]);
-    for (i = 0; i < 'h10; i++) begin
-        $dumpvars(0, u_System.u_DataMemory.memFile[i]);
-    end
+    for (i = 1; i < 32; i++) $dumpvars(1, `regFile[i]);
+    for (i = 0; i < 16; i++) $dumpvars(1, `memFile[i]);
 
     #2000;
-    `define memFile u_System.u_DataMemory.memFile
     `assert(`memFile[3], 45);
     $finish;
 end
