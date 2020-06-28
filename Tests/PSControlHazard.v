5eaf0ce54cee879be433ab93ced96cd52744f598
BugenZhao <i@bugenzhao.com>
2020-06-28 22:12:24 +0800

    Added: ControlHazard test

---
+++ Tests/PSControlHazard.v
@@ -0,0 +1,37 @@
+// -------------------------------------------------------
+// -- PipeSystem_tb.v
+// -------------------------------------------------------
+// Bugen Zhao 2020
+// -------------------------------------------------------
+
+`timescale 1ns / 1ps
+`include "ISA.v"
+`include "Debug.v"
+
+module PSControlHazard_tb;
+
+parameter textDump = "/Users/bugenzhao/Developer/Codes/Verilog/MIPS/Resources/Products/ControlHazard.mem";
+parameter PERIOD   = 10;
+
+PipeSystem #(textDump, PERIOD) u_PipeSystem();
+
+`define memFile u_PipeSystem.u_DataMemory.memFile
+`define regFile u_PipeSystem.u_PipeCPU.u_RegisterFile.regFile
+
+reg [`WORD] word;
+
+initial begin: test
+    integer i;
+
+    $dumpfile("wave.vcd");
+    $dumpvars;
+    for (i = 1; i < 32; i++) $dumpvars(1, `regFile[i]);
+
+    #1000;
+    `assert(`regFile[`T3], 1);
+    `assert(`regFile[`T4], 1);
+    
+    $finish;
+end
+
+endmodule // PSControlHazard_tb
